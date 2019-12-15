84356539756952e7f69d0fe257d958117215ca31
Stas Bes <forgame98@yandex.ru>
2019-12-15 04:51:22 +0300

    Added basic IN OUT animations/ and Splash screen

--- Assets/Scripts/Menu/ChangeScene.cs
+++ Assets/Scripts/Menu/ChangeScene.cs
@@ -12,17 +12,35 @@ public class ChangeScene : MonoBehaviour
     public int minPower = 100;
     public int maxPower = 100;
     public int lvl = 0;
-
-    Button button;
+    Button btn;
     private void Start()
     {
-        button = gameObject.GetComponent<Button>();
-        button.onClick.AddListener(changeScene);
+        btn = gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(onClick);
     }
 
-    void changeScene()
+    void onClick()
     {
         GameParams.initializeParams(mineCount, minPower, maxPower);
-        SceneManager.LoadScene(lvl);
+        StartCoroutine(LoadAsyncScene(lvl));
+    }
+    IEnumerator LoadAsyncScene(int lvl)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(lvl);
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            float progress = Mathf.Clamp01(asyncLoad.progress / 0.9f);
+            Debug.Log("Loading progress: " + (progress * 100) + "%");
+
+            // Loading completed
+            if (asyncLoad.progress == 0.9f)
+            {
+                asyncLoad.allowSceneActivation = true;
+            }
+            yield return null;
+
+        }
+
     }
 }
---
+++ Assets/Scripts/RequestChangeLvl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RequestChangeLvl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject preloader;
+    public int mineCount = 4;
+    public int minPower = 100;
+    public int maxPower = 100;
+    public int lvl = 0;
+    public void RequestLevel()
+    {
+        ChangeScene changeScene = preloader.GetComponent<ChangeScene>();
+        changeScene.mineCount = mineCount;
+        changeScene.minPower= minPower;
+        changeScene.maxPower = maxPower;
+        changeScene.lvl = lvl;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
